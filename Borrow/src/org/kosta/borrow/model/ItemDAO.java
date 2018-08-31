f166d35a8ffe8a5e828918e71f91e15800ac3615
kosta <kosta@kosta-HP>
2018-08-31 15:09:45 +0900

    정빈 삭제, 대여 추가

--- Borrow/src/org/kosta/borrow/model/ItemDAO.java
+++ Borrow/src/org/kosta/borrow/model/ItemDAO.java
@@ -8,6 +8,7 @@ import java.sql.SQLException;
 import javax.sql.DataSource;
 
 
+
 public class ItemDAO {
 	private static ItemDAO instance = new ItemDAO();
 	private DataSource dataSource;
@@ -20,9 +21,48 @@ public class ItemDAO {
 	public Connection getConnection() throws SQLException {
 		return dataSource.getConnection();
 	}
+	private void closeAll(PreparedStatement pstmt, Connection con) throws SQLException {
+		if(pstmt!=null)pstmt.close();
+		if(con!=null)con.close();
+	}
 	private void closeAll(ResultSet rs, PreparedStatement pstmt, Connection con) throws SQLException {
 		if(rs!=null)rs.close();
 		if(pstmt!=null)pstmt.close();
 		if(con!=null)con.close();
+	}
+	public void ItemDelete(ItemVO vo) throws SQLException {
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		try {
+			con=getConnection();
+			String sql = "UPDATE item SET item_status = 0 WHERE item_no = ?";
+			pstmt = con.prepareStatement(sql);
+			pstmt.setString(1, vo.getItemNo());
+			pstmt.executeUpdate();
+		}finally {
+			closeAll(pstmt, con);
+		}
+	}
+	
+	public void itemRental(RentalDetailVO vo) throws SQLException {
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		try {
+			con=getConnection();
+			String sql = "INSERT INTO rental_details (rental_no, item_no, id, rental_date, return_date) VALUES (rental_no_seq.nextval, ?, ?, ?, ?)";
+			pstmt = con.prepareStatement(sql);
+			pstmt.setString(1, vo.getItemVO().getItemNo());
+			pstmt.setString(2, vo.getMemberVO().getId());
+			pstmt.setString(3, vo.getRentalDate());
+			pstmt.setString(4, vo.getReturnDate());
+			pstmt.executeUpdate();
+			pstmt.close();
+			String sql2 = "update item_add set rental_count = RENTAL_count +1 where item_no = ?";
+			pstmt = con.prepareStatement(sql2);
+			pstmt.setString(1, vo.getItemVO().getItemNo());
+			pstmt.executeUpdate();
+		}finally {
+			closeAll(pstmt, con);
+		}
 	}
 }
