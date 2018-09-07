5c17b0bf4195c75dc3b40f75ed92301352ad9018
kosta <kosta@kosta-HP>
2018-09-07 12:10:35 +0900

    yosep - 버그 수정

--- Borrow/src/org/kosta/borrow/model/ItemDAO.java
+++ Borrow/src/org/kosta/borrow/model/ItemDAO.java
@@ -190,7 +190,7 @@ public class ItemDAO {
 		ResultSet rs = null;
 		try {
 			con = getConnection();
-			String sql="select count(*) from item where id=?";
+			String sql="select count(*) from item where id=? and item_status=1 and item_expdate>sysdate ";
 			pstmt = con.prepareStatement(sql);
 			pstmt.setString(1, id);
 			rs = pstmt.executeQuery();
@@ -402,7 +402,7 @@ public class ItemDAO {
 			sb.append(" select r.rnum, r.item_no, r.item_name, r.item_expl, r.item_price, r.id");
 			sb.append(" from (");
 			sb.append(" select row_number() over(order by item_regdate desc) as rnum, item_no, item_name, item_expl, item_price, id");
-			sb.append(" from item where id=?) r, member m");
+			sb.append(" from item where id=? and item_status=1 and item_expdate>sysdate ) r, member m");
 			sb.append(" where r.rnum between ? and ? and r.id=m.id");
 			sb.append(" order by item_no desc");
 			pstmt = con.prepareStatement(sb.toString());
@@ -872,7 +872,8 @@ public class ItemDAO {
 				RentalDetailVO rentalDetailVo= new RentalDetailVO();
 				rentalDetailVo.setRentalNo(rs.getString(1));
 				ItemVO item= new ItemVO();
-				item.setPicList(getPictureList(rs.getString(2)));				
+				item.setPicList(getPictureList(rs.getString(2)));			
+				item.setItemNo(rs.getString(2));
 				item.setItemName(rs.getString(3));
 				item.getMemberVO().setId(rs.getString(4));				
 				rentalDetailVo.setItemVO(item);
